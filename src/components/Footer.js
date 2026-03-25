93119d539c9c64d0d758d5ea2262947b6745de40
David <dsabalete@gmail.com>
2026-03-25 13:41:38 +0100

    Add "Buy Me A Coffee" link to footer and style adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,9 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="footer-signature">
+      <a href="https://buymeacoffee.com/dsabalete" target="_blank" rel="noopener noreferrer">
+        Buy Me A Coffee  ☕️
+      </a>
       <a href="https://github.com/dsabalete" target="_blank" rel="noopener noreferrer">
         Made with <span role="img" aria-label="love">❤️</span> by David Sabalete
       </a>
